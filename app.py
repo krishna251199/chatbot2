38145757a8a6ad9db0b77dd2421ef0dec842955c
krishna251199 <98869601+krishna251199@users.noreply.github.com>
2023-04-19 10:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,34 +2,27 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-#Flask initialization
+# Flask initialization
 app = Flask(__name__)
 
-chatbot=ChatBot('Pythonscholar')
+chatbot = ChatBot('Pythonscholar')
 
 # Create a new trainer for the chatbot
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Now let us train our bot with multiple corpus
 trainer.train("chatterbot.corpus.english.greetings",
-              "chatterbot.corpus.english.conversations" )
+              "chatterbot.corpus.english.conversations")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route('/app/get', methods=['POST'])
-def get():
-    # Your code to handle the request goes here
+@app.route("/get", methods=["POST"])
+def chatbot_response():
     msg = request.form["msg"]
     response = chatbot.get_response(msg)
     return str(response)
-    
 
-
-
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7303)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
