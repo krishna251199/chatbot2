d1d9f8d2ea7f70859a239e7ce19fe43cd3389c28
krishna251199 <98869601+krishna251199@users.noreply.github.com>
2023-04-19 09:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,13 @@ trainer.train("chatterbot.corpus.english.greetings",
 
 @app.route("/")
 def index():
-	
-	return render_template("index.html")
+    return render_template("index.html")
 
-
-
-@app.route("/get", methods=["GET","POST"])
+@app.route("/get", methods=["POST"])
 def chatbot_response():
     msg = request.form["msg"]
     response = chatbot.get_response(msg)
     return str(response)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
