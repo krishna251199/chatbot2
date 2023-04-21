2024ab534ec5b4eb04c04be9ad6644de2f605d2e
krishna251199 <98869601+krishna251199@users.noreply.github.com>
2023-04-21 15:52:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,105 @@
-from flask import Flask, render_template, request
+import nltk
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+
+import json
 import random
 
+
+
+
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+app.static_folder = 'static'
 
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/app/get")
+
 @app.route("/get")
 def get_bot_response():
-    user_text = request.args.get("msg")
-    if user_text.lower() == "hi" or user_text.lower() == "hello":
-        return "Hello! How can I assist you today?"
-    elif user_text.lower() == "what's your name?" or user_text.lower() == "what is your name?":
-        return "My name is Chatbot. How can I help you?"
-    elif user_text.lower() == "bye":
-        return "Goodbye! Have a nice day."
-    else:
-        responses = ["I'm sorry, I don't understand. Can you please rephrase?", 
-                     "I'm not sure what you mean. Can you provide more information?",
-                     "I'm sorry, I can't help you with that. Is there anything else I can assist you with?"]
-        return random.choice(responses)
+    
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+
+
+
+def clean_up_sentence(sentence):
+    model = load_model('model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('texts.pkl','rb'))
+    classes = pickle.load(open('labels.pkl','rb'))
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    model = load_model('model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('texts.pkl','rb'))
+    classes = pickle.load(open('labels.pkl','rb'))
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                # if show_details:
+                #     print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    model = load_model('model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('texts.pkl','rb'))
+    classes = pickle.load(open('labels.pkl','rb'))
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    model = load_model('model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('texts.pkl','rb'))
+    classes = pickle.load(open('labels.pkl','rb'))
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    model = load_model('model.h5')
+    intents = json.loads(open('intents.json').read())
+    words = pickle.load(open('texts.pkl','rb'))
+    classes = pickle.load(open('labels.pkl','rb'))
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
